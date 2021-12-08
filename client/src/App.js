8c1d5e7ce4f2ff48f4e30b9dc31bef18b600037b
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-12-07 20:10:46 -0800

    Basic styling for adding new passwords

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="AddingPassword">
+      <div className="AddPassword">
+          <input type="text" placeholder="Username"/>
           <input type="text" placeholder="Password"/>
           <input type="text" placeholder="Website"/>
           <button>Add Password</button>
