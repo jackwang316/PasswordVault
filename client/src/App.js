22e43ed8b0ede5cb34e1afaceae993022d130ced
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-12-07 19:58:42 -0800

    Initial frontend commit
    Initialized frontend with basic setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="AddingPassword">
+          <input type="text" placeholder="Password"/>
+          <input type="text" placeholder="Website"/>
+          <button>Add Password</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
