ec22cf9ccdfa5b2dc174cd54b29ac5eb1431b18f
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-12-07 20:17:57 -0800

    Tweaked styling and configured react states

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [website, setWebsite] = useState('')
   return (
     <div className="App">
       <div className="AddPassword">
-          <input type="text" placeholder="Username"/>
-          <input type="text" placeholder="Password"/>
-          <input type="text" placeholder="Website"/>
+          <input
+              type="text"
+              placeholder="Username"
+              onChange={(event) => {
+                  setUsername(event.target.value)
+              }}
+          />
+          <input
+              type="text"
+              placeholder="Password"
+              onChange={(event) => {
+                  setPassword(event.target.value)
+              }}
+          />
+          <input
+              type="text"
+              placeholder="Website"
+              onChange={(event) => {
+                  setWebsite(event.target.value)
+              }}
+          />
           <button>Add Password</button>
       </div>
     </div>
