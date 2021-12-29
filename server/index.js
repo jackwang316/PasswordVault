6ff369bcdbb4e24a1f44cb8a91f9685311a00bef
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2021-12-29 10:33:44 -0800

    Added post API to store password in db

--- server/index.js
+++ server/index.js
@@ -4,13 +4,31 @@ dotenv.config()
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.json())
 
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
     password: 'password',
     database: 'passwordmanager'
+});
+
+app.post('/addpassword', (req, res) => {
+    const {username, password, website} = req.body
+    db.query("INSERT INTO passwords (username, password, website) VALUES (?,?,?)", [
+        username,
+        password,
+        website,
+    ], (result, err) => {
+        if(err) {
+            console.error(err);
+        }else{
+            res.send("Account credentials added to database");
+        }
+    });
 });
 
 app.get('/', (req, res) => {
