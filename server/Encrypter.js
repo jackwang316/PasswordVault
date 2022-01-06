a6e63b84c0f4bec81491d9d2f35d939d962c5442
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2022-01-06 15:53:50 -0800

    Changed encryption algorithm

--- server/Encrypter.js
+++ server/Encrypter.js
@@ -4,7 +4,7 @@ const secret = '7yb4fVCL2gNz5cM24q^wU$ham9yHS*?a'
 const encrypt = (unencryptedVal) => {
     const iv = Buffer.from(crypto.randomBytes(16))
     const cipher = crypto.createCipheriv(
-        'aes-256-gcm',
+        'aes-256-ctr',
         Buffer.from(secret), iv
     )
     const encryptedVal = Buffer.concat([
@@ -19,16 +19,17 @@ const encrypt = (unencryptedVal) => {
 
 const decrypt = (encryptedVal) => {
     const decipher = crypto.createDecipheriv(
-        'aes-256-gcm',
+        "aes-256-ctr",
         Buffer.from(secret),
         Buffer.from(encryptedVal.iv, "hex")
-    )
-    const decryptedVal = Buffer.concat([
-        decipher.update(Buffer.from(encryptedVal.password, "hex")),
-        decipher.final(),
-    ])
+    );
 
-    return decryptedVal.toString()
+    const decryptedVal = Buffer.concat([
+        decipher.update(Buffer.from(encryptedVal.encryptedVal, "hex")),
+        decipher.final(),
+    ]);
+
+    return decryptedVal.toString();
 };
 
 module.exports = {encrypt, decrypt}
